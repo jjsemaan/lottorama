027bd7fd93b8ce27727340e7c98753b483f38724
jjsemaan <>
2023-07-29 15:29:47 +0000

    Move welcome note outside while loop

--- run.py
+++ run.py
@@ -18,27 +18,48 @@ SHEET = GSPREAD_CLIENT.open('lottorama-data')
 lotto_data = SHEET.worksheet('euro').get_all_values()
 
 # print(lotto_data)
-
+print()
+print("Welcome to Lottorama!")
+print("Let us help you win the Euro Millions jackpot.")
+print()
+print("Please enter your favorite Euro Millions ticket numbers.")
+print("Numbers should be five unique numbers separated by commas without spaces in between.")
+print("Example: 7,45,34,23,49\n")
 
 def validate_data(values):
     """
     Check if exactly 5 values are entered.
-    Check if values are strictly between 1 to 50
-    Check if these can be converted to integers
+    Check if values are strictly between 1 to 50.
+    Check if these can be converted to integers.
+    Check if the numbers are unique.
+    Check if there are no spaces between values and commas.
     """
     try:
+        # Check if there are spaces between values and commas
+        for value in values:
+            if ' ' in value:
+                raise ValueError("Spaces are not allowed between values and commas.")
+
         # Convert each value to an integer and check if they are within the range of 1 to 50
         int_values = [int(value) for value in values]
         for value in int_values:
             if not 1 <= value <= 50:
                 raise ValueError("Values should be between 1 and 50.")
-        
+
+        # Check if exactly 5 values are provided
         if len(values) != 5:
-            raise ValueError(
-                f"Exactly 5 values required and you provided only {len(values)}"
-            )
-    except ValueError as e:
-        print(f"Invalid data: {e}\n")
+            raise ValueError("Exactly 5 values required.")
+            
+        # Check if the numbers are unique
+        if len(set(int_values)) != 5:
+            raise ValueError("The 5 numbers should be unique.")
+            
+    except ValueError:
+        print()
+        print("* Please check your numbers and try again!")
+        print("* You should provide exactly 5 unique whole numbers.") 
+        print("* Numbers should be strictly between 1 and 50 with commas in between and no spaces (Example: 7,45,34,23,49).")
+        print()
         return False
    
     return True
@@ -49,32 +70,19 @@ def get_lotto_data():
     Get lotto figures input from the user
     Run while loop until correct data is input
     """
+
+    # Print the last draw date and winning numbers only once at the start
+    euro = SHEET.worksheet("euro").get_all_values()
+    last_draw = euro[-1]
+    print(f"Last draw date: {last_draw[0]}")
+    
+    winning_numbers_str = ""
+    for number in last_draw[1:6]:
+        winning_numbers_str += number + ' '
+    print(f"Winning numbers: {winning_numbers_str}\n")
+
     while True:
-        print()
-        print("Welcome to Lottorama!")
-        print("Let us help you win the Euro Millions jackpot.")
-        print()
-
-        euro = SHEET.worksheet("euro").get_all_values()
-        last_draw = euro[-1]
-
-        # Print the first element of last_draw (index 0)
-        print(f"Last draw date: {last_draw[0]}")
-
-        # Print the rest of the elements in the list (index 1 to 5)
-        winning_numbers_str = ""
-        for number in last_draw[1:6]:
-            winning_numbers_str += number + ' '
-
-        # Print the winning numbers on the same line
-        print(f"Winning numbers: {winning_numbers_str}")
-        print()  # Add an empty row
-
-        print("Please enter your favorite Euro Millions ticket numbers.")
-        print("Numbers should be five numbers separated by commas.")
-        print("Example: 7,45,34,23,49\n")
-        print()  # Add an empty row
-
+        
         data_str = input("Enter your data here:\n")
         lotto_data = data_str.split(",")
 
